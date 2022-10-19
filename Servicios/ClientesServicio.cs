8a34798cf48256d3309d88643ecfa30ad447df5c
ivanortiz <ivanlortiz2001@gmail.com>
2022-10-19 09:10:13 -0300

    agregado eliminar y modificar clientes

--- Servicios/ClientesServicio.cs
+++ Servicios/ClientesServicio.cs
@@ -7,6 +7,9 @@ namespace LorenzoApplication.Servicios
     {
         public Task<List<Cliente>> Listar();
         public Task<bool> AgregarCliente(Cliente cliente);
+        public Task<Cliente> Leer(string cliente);
+        public Task<bool> Eliminar(string codigo);
+        public Task<bool> Modificar(Cliente cliente);
     }
     public class ClientesServicio : IClientesServicio
     {
@@ -32,6 +35,44 @@ namespace LorenzoApplication.Servicios
         {
             var respuesta = await Db.Clientes.ToListAsync();
             return respuesta;
+        }
+
+        public async Task<Cliente?> Leer(string cliente)
+        {
+            var respuesta = await Db.Clientes.Where(x => x.Codigo.Equals(cliente)).FirstOrDefaultAsync();
+            return respuesta;
+
+        }
+
+        public async Task<bool> Eliminar(string codigo)
+        {
+            var respuesta = await Db.Clientes.Where(x => x.Codigo.Equals(codigo)).FirstOrDefaultAsync();
+            if (respuesta == null) return false;
+            else
+            {
+                Db.Clientes.Remove(respuesta);
+                await Db.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async Task<bool> Modificar(Cliente cliente)
+        {
+            var respuesta = await Db.Clientes.Where(x => x.Codigo.Equals(cliente.Codigo)).FirstOrDefaultAsync();
+            if (respuesta == null) return false;
+            else
+            {
+                respuesta = Normalizar(respuesta);
+                Db.Clientes.Update(respuesta);
+                await Db.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        static private Cliente Normalizar(Cliente dato)
+        {
+            dato.Nombre = dato.Nombre.ToUpper();
+            return dato;
         }
     }
 }
